bfecfe6b503d2b694ce5bc22af89c1e27697a2d0
manalijoshi00 <108450658+manalijoshi00@users.noreply.github.com>
2022-07-29 12:03:17 +0530

    Add files via upload

---
+++ Thursday Lab/Practical_28-07-2022/TextFileReader.java
@@ -0,0 +1,25 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+public class TextFileReader
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			FileInputStream input = new FileInputStream("Files.txt");
+                  int i = input.read();
+                  input.skip(4);
+                  System.out.println("Input Stream after skipping 4 byte :");
+			while(i != -1)
+			{
+				System.out.println((char) i);
+                        i = input.read();
+			}
+                  input.close();
+		}
+		catch(IOException ex)
+		{
+			 ex.printStackTrace();
+		}
+	}
+}
