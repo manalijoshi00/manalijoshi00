2b2998233054c900eab43c94eb568fa75ac4b72d
manalijoshi00 <108450658+manalijoshi00@users.noreply.github.com>
2022-07-29 12:05:13 +0530

    Update TextFileReader.java

--- Thursday Lab/Practical_28-07-2022/TextFileReader.java
+++ Thursday Lab/Practical_28-07-2022/TextFileReader.java
@@ -7,15 +7,15 @@ public class TextFileReader
 		try
 		{
 			FileInputStream input = new FileInputStream("Files.txt");
-                  int i = input.read();
-                  input.skip(4);
-                  System.out.println("Input Stream after skipping 4 byte :");
+                        int i = input.read();
+                        input.skip(4);
+                        System.out.println("Input Stream after skipping 4 byte :");
 			while(i != -1)
 			{
 				System.out.println((char) i);
-                        i = input.read();
+                                i = input.read();
 			}
-                  input.close();
+                        input.close();
 		}
 		catch(IOException ex)
 		{
