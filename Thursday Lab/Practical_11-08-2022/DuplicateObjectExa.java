58451fc0a4e5a8074be10131db3ad188bcff042a
manalijoshi00 <108450658+manalijoshi00@users.noreply.github.com>
2022-08-11 15:24:27 +0530

    Add files via upload

---
+++ Thursday Lab/Practical_11-08-2022/DuplicateObjectExa.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
+class DuplicateObjectExa
+{
+      public static void main(String[] args)
+      {
+            ArrayList<String> list = new ArrayList<String>();
+            list.add("Jan");
+            list.add("Feb");
+            list.add("Mar");
+            list.add("Jan");
+            list.add("Feb");
+            list.add("Apr");
+            list.add("May");
+            System.out.println("List : " + list);
+            Set<String> set = new LinkedHashSet<String>(list);
+            System.out.println("List after removing duplicate elements = "+set);
+   }
+}
