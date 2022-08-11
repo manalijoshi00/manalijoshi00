84cf4f403de7b57f1168de5735a5f610d41113c1
manalijoshi00 <108450658+manalijoshi00@users.noreply.github.com>
2022-08-11 14:03:03 +0530

    Add files via upload

---
+++ Thursday Lab/Practical_11-08-2022/ArrayListExample.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+class ArrayListExample
+{
+     public static void main(String[] args)
+     {
+           ArrayList<String> arr = new ArrayList<String>();
+           arr.add("Red");
+           arr.add("Pink");
+           arr.add("Green");
+           arr.add("Blue");
+           arr.add("Yellow");
+           System.out.println(arr);
+           arr.remove(3);
+           System.out.println("After Removing :");
+           System.out.println(arr);
+           System.out.println("The element at 2nd index is: "+ arr.get(2));
+        }
+}
